857578d30b2e3913309a30d293279f963fa93de1
nelsonblaha <nelson.blaha@gmail.com>
2012-10-16 19:36:28 -0500

    blank rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blaha::Application.config.secret_token = 'ae7a908e14d504dfebb438ff8dbc5b3fd6b23766066a4c30f6ac27fe08d2d0c88b7943c87cf4ae1e835b19ba6e69694c96b00c0bd9c5bfcad3e3f7b76c039863'
