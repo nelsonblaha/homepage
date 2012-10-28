90266a593353d5dcc62144cfb41c275181c8bfb0
nelsonblaha <nelson.blaha@gmail.com>
2012-10-27 19:28:31 -0500

    icon url method for link

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,12 @@ class Link < ActiveRecord::Base
   attr_accessible :description, :title, :url
   has_many :taggings, foreign_key: :item_id, dependent: :destroy
   has_many :tags, through: :taggings
+
+  def icon
+  	if self.url.scan(/github.com/).count > 0
+  		return "octocat.png"
+  	else
+  		return "generic.png"
+  	end
+  end
 end
